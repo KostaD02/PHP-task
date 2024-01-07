8750498be9734dd0d4e02a2150b9becdc4875996
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-01-07 16:40:05 +0400

    feat: wrap in base template for header/footer

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="./assets/styles/style.css">
+  <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/68782786?v=4" type="image/x-icon">
 </head>
 
 <body>
@@ -22,9 +23,17 @@
   if (!in_array($page, $allowedPages)) {
     $page = 'main';
   }
-  include './assets/pages/' . $page . '.php';
-  include './assets/components/footer.php';
   ?>
+  <main class="container-xxl m-auto">
+    <?php
+    include './assets/pages/' . $page . '.php';
+    ?>
+  </main>
+  <footer class="d-flex justify-content-center align-items-center bg-body-tertiary">
+    <?php
+    include './assets/components/footer.php';
+    ?>
+  </footer>
   <script src="https://kit.fontawesome.com/af6bc85f41.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
